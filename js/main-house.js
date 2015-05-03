4920e818f2d9b69e46e781a600a07ef7556e6e54
Margie Roswell <mroswell@gmail.com>
2015-05-03 18:20:23 -0400

    start house map

---
+++ js/main-house.js
@@ -0,0 +1,170 @@
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0Ao3Ts9D8bHHpdDlXWVlhSl9UelVmSjV0NHJBUGxQbFE&output=html';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1fv-XHYGddsiirJaXMIP8A-TK36xfssjCnifWEzcJltc/pubhtml';
+var dist;
+var freeze;
+var MDSenateDistricts = {};
+var app = {};
+var senateLayer;
+var latitude = 38.9;
+var longitude = -77.28;
+var latLng = new L.LatLng(latitude, longitude);
+var sidebar = $('#sidebar');
+var map = L.map('map').setView(latLng, 8);
+
+
+
+function clearInfobox() {
+  sidebar.html(' ');
+}
+
+$(document).ready(function() {
+  Tabletop.init({
+    key: public_spreadsheet_url,
+    callback: showInfo,
+    parseNumbers: true
+  });
+  console.log(public_spreadsheet_url);
+});
+
+function showInfo(data, tabletop) {
+  console.log("here");
+  console.log("here1");
+  var scoreColor;
+  var source = $("#senate-template").html();
+  var template = Handlebars.compile(source);
+  var sourcebox = $("#senate-template-infobox").html();
+  app.infoboxTemplate = Handlebars.compile(sourcebox);
+  $.each(tabletop.sheets("House2015").all(), function(i, member) {
+    scoreColor = getColor(member.score2015);
+
+    member['scoreColor'] = scoreColor;
+    var html = template(member);
+    $("#content").append(html);
+    MDSenateDistricts[member.district] = member;
+  });
+  console.log(MDSenateDistricts);
+  loadGeo();
+  //          processJSON(tabletop.sheets("Sheet1").all());
+}
+function getColor(score) {
+  return score > 80 ? '#4EAB07' :
+    score > 60 ? '#82e0c3' :
+      score > 40 ? '#FEF200' :
+        score > 20 ? '#FDC300' :
+          score > 0 ? '#FC8400' :
+            'rgb(255,0,0)';
+}
+
+var geoStyle = function(data) {
+  var rownum = Number(data.properties.rownum);
+  // Sort out how to get at the SLDUST data.
+  // console.log("geoStyle: ", Number(data.properties.SLDUST));
+ console.log(rownum);
+  var score = MDSenateDistricts[rownum].score2015;
+  var scoreToColor = getColor(score);
+
+  return {
+    radius: 6,
+    fillColor: scoreToColor,
+    weight: 2,
+    opacity: 0.51,
+    color: '#fff',
+    dashArray: '0',
+    fillOpacity: 1
+  }
+};
+
+
+function loadGeo(district) {
+  var Hydda_Full = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+    minZoom: 0,
+    maxZoom: 9,
+    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  senateBoundaryLayer = L.geoJson(mdSenateBoundary, {
+  }).addTo(map).setStyle({
+      stroke: true,
+      fillColor:'#ffffff',
+      color: '#3f3f3f',
+      weight: 1,
+      fillOpacity:.85
+    });
+
+  senateLayer = L.geoJson(geosenate, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, geoStyle(feature));
+    }
+  }).addTo(map);
+}
+
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: mapMemberDetailClick
+  });
+}
+
+
+function highlightFeature(e) {
+  var layer = e.target;
+  var memberNumber = Number(layer.feature.properties.SLDUST);
+  var memberDetail = MDSenateDistricts[memberNumber];
+
+  // This fills in the sidebar as you mouse around.
+  // NO STICKY CLICK
+
+  if (!freeze) {
+    html = app.infoboxTemplate(memberDetail);
+    $('#sidebar').html(html);
+
+    layer.setStyle({
+      weight: 2,
+      color: '#fff',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
+    }
+  }
+}
+
+function resetHighlight(e) {
+  var layer = e.target;
+  senateLayer.resetStyle(e.target);
+
+  if (!freeze) {
+    clearInfobox();
+  }
+}
+
+function mapMemberDetailClick(e) {
+  freeze = 1;
+  var boundary = e.target;
+  var memberNumber = Number(boundary.feature.properties.SLDUST);
+  console.log("mapMemberDetailClick: ", memberNumber);
+  var member = memberDetailFunction(memberNumber);
+}
+
+
+function memberDetailFunction(memberNumber) {
+  var districtDetail = MDSenateDistricts[memberNumber]
+  var html = app.infoboxTemplate(districtDetail);
+  $('#sidebar').html(html);
+  // $('#sidebar').html(JSON.stringify(districtDetail));
+}
+
+
+
+
+$(document).on("click", "button", function(event) {
+  event.preventDefault();
+  clearInfobox();
+  freeze = 0;
+});
